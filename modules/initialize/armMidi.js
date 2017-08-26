96c64e88ccd6f6a5b6ea9bf79236de0a2fc8c05c
August Conwell <august.conwell@gmail.com>
2017-08-26 16:36:23 -0400

    Parse midi events with midimessage

--- modules/initialize/armMidi.js
+++ modules/initialize/armMidi.js
@@ -1,9 +1,11 @@
+const MIDIMessage = require('midimessage');
+
 module.exports = (MIDI) => {
     console.log(MIDI);
     MIDI.inputs.map(portAndID => {
         const port = portAndID[1];
         console.log('id:', port.id, 'manufacturer:', port.manufacturer, 'name:', port.name, 'version:', port.version);
-        port.onmidimessage = (message) => console.log(message);
+        port.onmidimessage = (message) => console.log(MIDIMessage(message));
     })
     return
 }
