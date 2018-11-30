4eebb8996c8314d9a53151411f2521d81314b90b
August Conwell <august.conwell@gmail.com>
2018-11-29 20:08:50 -0500

    nice!

--- scratch.js
+++ scratch.js
@@ -1,15 +1,6 @@
-const render = require('./utils/render');
-
-const menuList = require('./views/dynamic/menu-list');
-
-render(menuList([
-  { text: 'hey', isActive: false },
-  { text: 'heeeey', isActive: true },
-]));
-
-setTimeout(() => render(menuList([
-  { text: 'hey', isActive: true },
-  { text: 'heeeey', isActive: false },
-])), 1000);
-
-setTimeout(() => { }, 4000);
+/**
+ * index.js is user config + transport timer
+ * each route forks a route process
+ * timer sends each route ticks and midi messages
+ * each route sends back midi messages
+ */
